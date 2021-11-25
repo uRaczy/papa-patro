4196b9d726fc62ae51b12f755d5dafec70c24e0d
uRaczy <raczy7@gmail.com>
2021-11-25 15:35:35 +0100

    Add basic fetch in promise

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,13 @@
-const navbar = document.querySelector('navbar');
+const makeMenu = async () => {
+  const pizzasList = await fetchPizzas;
+  
+}
 
-console.log(navbar);
+const fetchPizzas = new Promise((resolve, reject) => {
+  fetch('https://raw.githubusercontent.com/alexsimkovich/patronage/main/api/data.json')
+    .then(res => res.json())
+    .then(data => resolve(data))
+    .catch(err => console.error(err));
+});
+
+getList();
